9b43e915f9939fe12e47c8ffcc25d2d3ffc356b1
GabrielGomesBarbosa <gabriel-gomes-barbosa@hotmail.com>
2024-10-20 00:29:00 -0300

    feat: create basic route structure for crops

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 
+import { router } from './routes'
+
 const HOST = process.env.HOST
 const PORT = process.env.PORT || 3333
 
 const server = express()
 
 server.use(express.json())
+
+server.use(router)
 
 server.listen(PORT, () => console.log(`Server running on: ${HOST}:${PORT}`))
---
+++ backend/src/routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express'
+
+const router = Router()
+
+router.post('/crops/filter', (_, response) => {
+  response.status(200).send();
+})
+
+router.get('/crops/:id', (_, response) => {
+  response.status(200).send();
+})
+
+router.post('/crops', (_, response) => {
+  response.status(200).send();
+})
+
+router.delete('/crops/:id', (_, response) => {
+  response.status(200).send();
+})
+
+export { router }
