887691f1f12a358d9e6fa3015616a3569d817ac9
GabrielGomesBarbosa <gabriel-gomes-barbosa@hotmail.com>
2024-10-20 00:16:12 -0300

    feat: change es2016 to es2017, remove strict mode and include json middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 
-const HOST = process.env.HOST;
-const PORT = process.env.PORT || 3333;
+const HOST = process.env.HOST
+const PORT = process.env.PORT || 3333
 
 const server = express()
+
+server.use(express.json())
 
 server.listen(PORT, () => console.log(`Server running on: ${HOST}:${PORT}`))
